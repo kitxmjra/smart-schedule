e889ea4afbfdca08b454edf5dc1f89f69dc47499
kitxmjra <potapov.egor3231@gmail.com>
2026-01-25 15:24:30 +0300

    Update main.c

--- src-c/main.c
+++ src-c/main.c
@@ -8,9 +8,13 @@ int main() {
     
     Time start = {10, 0};
     Time end = {11, 0};
+    Time endl = {13, 00};
+    Time statr = {12, 00};
     Lesson math = create_Lesson(1, "Math", 20, start, end);
+    Lesson language = create_Lesson(2, "English Language", 10, statr, endl);
     init_schedule(&monday, "Monday");
     add_lesson(&monday, &math);
+    add_lesson(&monday, &language);
     print_day(&monday);
     return 0;
 }
